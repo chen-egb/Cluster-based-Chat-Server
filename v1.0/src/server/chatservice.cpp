d0277e8dde90b05164cb26d47feb5059dee39b2e
XiaoHange <1423867677@qq.com>
2026-04-06 14:39:27 +0800

    实现用户登录注册功能

---
+++ v1.0/src/server/chatservice.cpp
@@ -0,0 +1,152 @@
+#include "chatservice.hpp"
+#include "public.hpp"
+#include <muduo/base/Logging.h>
+using namespace muduo;
+
+// 获取单例对象的接口函数
+ChatService *ChatService::instance()
+{
+    static ChatService service;
+    return &service;
+}
+
+// 注册消息以及对应的Handler回调操作
+ChatService::ChatService()
+{
+    _msgHandlerMap.insert({LOGIN_MSG, std::bind(&ChatService::login, this, _1, _2, _3)});
+    _msgHandlerMap.insert({REG_MSG, std::bind(&ChatService::reg, this, _1, _2, _3)});
+}
+
+// 获取消息对应的处理器
+MsgHandler ChatService::getHandler(int msgid)
+{
+    // 记录错误日志，msgid没有对应的事件处理回调
+    auto it = _msgHandlerMap.find(msgid);
+    if (it == _msgHandlerMap.end())
+    {
+        // 返回一个默认的处理器，空操作
+        return [=](const TcpConnectionPtr &conn, json &js, Timestamp)
+        {
+            LOG_ERROR << "msgid:" << msgid << " can not find handler!";
+        };
+    }
+    else
+    {
+        return _msgHandlerMap[msgid];
+    }
+}
+
+// 处理登录业务
+void ChatService::login(const TcpConnectionPtr &conn, json &js, Timestamp time)
+{
+    int id = js["id"].get<int>();
+    string pwd = js["password"];
+
+    User user = _userModel.query(id);
+    if (user.getId() == id && user.getPwd() == pwd)
+    {
+        if (user.getState() == "online")
+        {
+            // 该用户已经登录，不允许重复登录
+            json response;
+            response["msgid"] = LOGIN_MSG_ACK;
+            response["errno"] = 2;
+            response["errnomsg"] = "该账号已经登录，请重新输入新账号";
+            conn->send(response.dump());
+        }
+        else
+        {
+            // 登录成功，记录用户连接信息
+            {
+                lock_guard<mutex> lock(_connMutex);
+                _userConnMap.insert({id, conn});
+            }
+
+            // 登录成功，更新用户状态信息
+            user.setState("online");
+            _userModel.updateState(user);
+
+            json response;
+            response["msgid"] = LOGIN_MSG_ACK;
+            response["errno"] = 0;
+            response["id"] = user.getId();
+            response["name"] = user.getName();
+            conn->send(response.dump());
+        }
+    }
+    else
+    {
+        // 该用户不存在，用户存在但是密码错误，登录失败
+        if (user.getId() == id)
+        {
+            json response;
+            response["msgid"] = LOGIN_MSG_ACK;
+            response["errno"] = 1;
+            response["errnomsg"] = "用户名或者密码错误";
+            conn->send(response.dump());
+        }
+        else
+        {
+            json response;
+            response["msgid"] = LOGIN_MSG_ACK;
+            response["errno"] = 3;
+            response["errnomsg"] = "用户不存在";
+            conn->send(response.dump());
+        }
+    }
+}
+
+// 处理注册业务
+void ChatService::reg(const TcpConnectionPtr &conn, json &js, Timestamp time)
+{
+    string name = js["name"];
+    string pwd = js["password"];
+
+    User user;
+    user.setName(name);
+    user.setPwd(pwd);
+    bool state = _userModel.insert(user);
+    if (state)
+    {
+        // 注册成功
+        json response;
+        response["msgid"] = REG_MSG_ACK;
+        response["errno"] = 0;
+        response["id"] = user.getId();
+        conn->send(response.dump());
+    }
+    else
+    {
+        // 注册失败
+        json response;
+        response["msgid"] = REG_MSG_ACK;
+        response["errno"] = 1;
+        conn->send(response.dump());
+    }
+}
+
+// 处理客户端异常退出
+void ChatService::clientCloseExpection(const TcpConnectionPtr &conn)
+{
+    User user;
+    {
+        lock_guard<mutex> lock(_connMutex);
+        for (auto it = _userConnMap.begin(); it != _userConnMap.end(); it++)
+        {
+            if (it->second == conn)
+            {
+                // 从map表删除用户的链接信息
+                user.setId(it->first);
+                _userConnMap.erase(it);
+                break;
+            }
+        }
+    }
+
+    // 更新用户的状态信息
+    if (user.getId() != -1)
+    {
+        user.setState("offline");
+        _userModel.updateState(user);
+    }
+}
